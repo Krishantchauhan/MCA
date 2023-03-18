8348a72adbae54b4cbdc6b0eddf3c3fcca8b2172
Krishantchauhan <krashantchauhan@gmail.com>
2023-03-18 11:25:52 +0530

    rotate

--- Sem2/Placement/Array/rotate.c
+++ Sem2/Placement/Array/rotate.c
@@ -24,15 +24,18 @@ int main()
     int a[] = {1, 2, 3, 4, 5};
     int n = sizeof(a) / sizeof(a[0]);
 
-    int k = 4;
+    int k = 1;
     while (k--)
     {
         int temp = a[0];
         for (int i = 0; i < n - 1; i++)
+        {
             a[i] = a[i + 1];
+            printf("%d  ", a[i]);
+        }
         a[n - 1] = temp;
     }
 
-    for (int i = 0; i < n; i++)
-        printf("%d  ", a[i]);
+    // for (int i = 0; i < n; i++)
+    // printf("%d  ", a[i]);
 }
