c1a67469e019552de65f17ef2a15509fea452974
Krishantchauhan <krashantchauhan@gmail.com>
2023-04-05 19:26:04 +0530

    factorial using cmd line args
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

---
+++ Sem2/Java/Factorial/fact.java
@@ -0,0 +1,14 @@
+// Write a java program find factorial of a Number should be entered through command line
+
+class fact
+{
+    public static void main(String[] args) {
+        int f = 1;
+        int n = Integer.parseInt(args[0]);
+        while (n > 0) {
+            f = f * n;
+            n--;
+        }
+        System.out.println("factorial: " + f);
+    }
+}
