66ffd1b9ddfc39436336101a955c434cfa49ea53
Krishantchauhan <krashantchauhan@gmail.com>
2023-05-29 17:21:56 +0530

    Bin search
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- Sem2/DSA/Sorting/4-Binary_search.c
+++ Sem2/DSA/Sorting/4-Binary_search.c
@@ -27,7 +27,14 @@ int main()
 {
     int arr[] = {1, 3, 5, 7, 9};
     int n = sizeof(arr) / sizeof(arr[0]);
-    int key = 5;
+    printf("Array: \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    int key;
+    printf("\nEnter the Element to search\n");
+    scanf("%d", &key);
 
     int index = binarySearch(arr, 0, n - 1, key);
 
