153c740aaab98864ccf6224798b699b4691d4876
Krishantchauhan <krashantchauhan@gmail.com>
2023-06-07 17:05:02 +0530

    concatinate string
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

---
+++ Sem2/Java/After-mid/src/concatenateStrings.java
@@ -0,0 +1,20 @@
+public class concatenateStrings {
+    public static String concatenate(String str1, String str2) {
+        String result = str1 + str2;
+
+        if (result.length() >= 2 && result.charAt(0) == result.charAt(1)) {
+            return result.substring(1).toLowerCase();
+        } else {
+            return result.toLowerCase();
+        }
+    }
+
+    public static void main(String[] args) {
+        String input1 = "Kris";
+        String input2 = "iya";
+
+        String result = concatenate(input1, input2);
+
+        System.out.println("Result: " + result);
+    }
+}
