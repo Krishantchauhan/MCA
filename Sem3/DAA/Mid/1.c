34f538682260d2c1e91cf7f06b46fd9aafeeb0d7
Krishantchauhan <krashantchauhan@gmail.com>
2023-09-27 18:27:41 +0530

    1
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

---
+++ Sem3/DAA/Mid/1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+
+    while (a)
+    {
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            scanf("%d", &arr[i]);
+
+        int key;
+        scanf("%d", &key);
+        int flag = 0;
+        int pos = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (key == arr[i])
+            {
+                flag = 1;
+                pos = i + 1;
+                break;
+            }
+            else
+                pos = i + 1;
+        }
+        if (flag)
+            printf("Present %d \n", pos);
+        else
+            printf("Not Present %d \n", pos);
+        a--;
+    }
+}
